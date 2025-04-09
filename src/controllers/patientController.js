15b56dcb449b470ac9d29a4756faf5c841bc98a1
jonwwick <tuandtuan4@gmail.com>
2025-04-09 18:56:47 +0700

    update booking

--- src/controllers/patientController.js
+++ src/controllers/patientController.js
@@ -4,7 +4,7 @@ let postBookAppointment = async (req, res) => {
 
     try {
         let infor = await patientService.postBookAppointment(req.body);
-        return res.status(200).json(infor);
+        return res.status(200).json({errCode: 0,infor});
     } catch (e) {
         console.log(e);
         return res.status(200).json({
